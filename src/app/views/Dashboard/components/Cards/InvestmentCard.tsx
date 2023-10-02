5c16000f5a0a1c14de8c703f039be70b1187867d
Kitan Garcia <kitangar@gmail.com>
2023-10-02 14:45:40 -0700

    make investment cards larger for large screen view
    add padding to bottom of investment cards for mobile view

--- src/app/views/Dashboard/components/Cards/InvestmentCard.tsx
+++ src/app/views/Dashboard/components/Cards/InvestmentCard.tsx
@@ -7,23 +7,23 @@ interface InvestmentCardProps {
 function InvestmentCard({ name, imageSrc, position }: InvestmentCardProps) {
   const cardMargins: Record<string, Record<string, string>> = {
     standard: {
-      0: "mr-8",
-      1: "mx-8",
-      2: "ml-8",
+      0: "mr-12",
+      1: "mx-12",
+      2: "ml-12",
     },
     mobile: {
       0: "mobile:mb-10 mobile:mx-0",
       1: "mobile:my-10 mobile:mx-0",
-      2: "mobile:mt-10 mobile:mx-0",
+      2: "mobile:mt-10 mobile:mb-20 mobile:mx-0",
     },
   };
   return (
     <div
       className={`border border-[#29231B] whitespace-nowrap rounded-2xl 
-    lg: w-[231px] h-full flex flex-col justify-center cursor-pointer ${cardMargins.standard[position]} ${cardMargins.mobile[position]} mobile:w-auto`}
+    lg: h-full flex flex-col justify-center cursor-pointer ${cardMargins.standard[position]} ${cardMargins.mobile[position]} mobile:w-auto`}
     >
       <img
-        className="rounded-t-2xl w-[235px] h-[228px] mobile:w-[314px] mobile:h-[304px]"
+        className="rounded-t-2xl w-[314px] h-[304px]"
         src={imageSrc}
         alt=""
       />
