5848250e9373180f83d67a50ea6f549e5b451c0b
BLR_01 <110558156+blockrideNFT@users.noreply.github.com>
2024-08-19 04:47:20 +0100

    Update request.ts

--- src/app/api/request.ts
+++ src/app/api/request.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export default Axios.create({
  // baseURL: "https://melodic-zebra-production.up.railway.app",
-  baseURL:"http://104.251.217.197:3001"
+  baseURL:"http://104.251.217.197:3001",
   headers: {
     "Content-Type": "application/json",
   },
